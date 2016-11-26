e82df3317cc14b1c640a4f16f875e14b6e6cf9f7
boke1208 <907886076@qq.com>
2016-11-26 13:36:44 +0800

    log

--- client.cpp
+++ client.cpp
@@ -1,15 +1,17 @@
-/***************
+/**********************
 
-    client.c
+     client.cpp
 
-***************/
+        by:sissel
+
+**********************/
 #include <iostream>
 #include <cstdio>
 #include <WinSock2.h>
 #include <windows.h>
 #pragma comment(lib, "ws2_32.lib")  //加载 ws2_32.dll
 
-#define BUF_SIZE 100000
+#define BUF_SIZE 1000000
 #define LOCAL_PORT 200
 #define SERV_ADD "127.0.0.1"
 #define SERV_PORT 300
@@ -51,7 +53,8 @@ int main(){
 
     char bufRecv[BUF_SIZE] = {0};//缓存区
 
-    while(1){
+    while(1)
+    {
         //监听本地内容
         SOCKET clntSock = accept(localSock, (SOCKADDR*)&clntAddr, &nSize);
         int str_len_local = recv(clntSock, buffer, BUF_SIZE, 0);
@@ -67,9 +70,8 @@ int main(){
         //接收服务器传回的数据
         int str_len = recv(sock, bufRecv, BUF_SIZE, 0);
         //输出接收到的数据
-        cout << bufRecv;
         send(clntSock, bufRecv, str_len, 0);  //将数据原样返回给其它应用
-
+        cout << bufRecv;
         //重置缓冲区
         memset(bufRecv, 0, BUF_SIZE);
         memset(buffer, 0, BUF_SIZE);
--- server.cpp
+++ server.cpp
@@ -1,11 +1,14 @@
-/*************
+/***********************
 
-   server.c
+     server.cpp
 
-*************/
+        by:sissel
+
+***********************/
 
 #include <iostream>
 #include <stdio.h>
+#include <string.h>
 #include <winsock2.h>
 #pragma comment (lib, "ws2_32.lib")  //加载 ws2_32.dll
 
@@ -13,6 +16,79 @@
 #define SERV_PORT 300
 
 using namespace std;
+
+int get_requests_info(char *buffer, char *ip, int *port)
+{
+    int i;
+    char m_port[] = "HTTP/";
+    char m_http[] = "http://";
+    char m_https[] = "https://";
+    char m_host[] = "Host: ";
+    char address[1000];//域名
+    char host[500];
+    char *ptr;
+    *port = 0;
+    //域名
+    /*
+    ptr = strchr(buffer, ' ');
+    if(ptr==NULL)
+    {
+        cout << "error requests************************" <<endl;
+        return 2;
+    }
+    else if(strstr(buffer,m_http)!=NULL)
+        ptr += 7;
+    else if(strstr(buffer,m_https)!=NULL)
+        ptr += 8;
+
+    for(i = 0, ptr+=1;*ptr != ' ';ptr++,i++)
+    {
+        address[i] = *ptr;
+    }
+    address[i] = '\0';
+    */
+
+    //HOST
+    ptr = strstr(buffer, m_host);
+    if(ptr==NULL)
+    {
+        cout << "error host----------do not have host:" << endl;
+        return -1;
+    }
+    for(i = 0, ptr += 6;*ptr != '\r' && *ptr != ':';ptr++,i++)
+    {
+        host[i] = *ptr;
+    }
+    if(*ptr == ':')
+    {
+        for(ptr+=1;*ptr!='\r'&&*ptr!=' ';ptr++)
+        {
+            *port *= 10;
+            *port += (*ptr-'0');
+        }
+    }
+    host[i] = '\0';
+
+    //端口
+    ptr = strstr(buffer, m_port);
+    if(*port==0)
+        if(ptr!=NULL)
+            *port = 80;
+        else
+            *port = 443;
+
+    struct hostent *host_addr = gethostbyname(host);
+    if (host_addr == NULL)
+    {
+        cout << "host error ----------- cannot use gethostbyname()" <<endl;
+        return -1;
+    }
+    memset(host, 0, sizeof(host));
+    strcpy(host, host_addr->h_name);
+    strcpy(ip, inet_ntoa( *(struct in_addr*)host_addr->h_addr_list[0] ));
+
+    return 1;
+}
 
 int main(){
     WSADATA wsaData;
@@ -38,34 +114,49 @@ int main(){
     char buffer[BUF_SIZE] = {0};  //缓冲区
     char buf_recv[BUF_SIZE] = {0};//缓存区
 
-    while(1){
+    while(1)
+    {
         SOCKET clntSock = accept(servSock, (SOCKADDR*)&clntAddr, &nSize);
         int str_len_recv = recv(clntSock, buffer, BUF_SIZE, 0);  //接收客户端发来的数据
+
         cout << buffer;
-        //send(clntSock, buffer, strLen, 0);  //将数据原样返回
 
-        //服务器发送请求
-        SOCKET sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+        char ip[30];
+        int port;
 
-        sockaddr_in servSockAddr;
-        memset(&servSockAddr, 0, sizeof(servSockAddr));
-        servSockAddr.sin_family = PF_INET;
-        servSockAddr.sin_addr.s_addr = inet_addr("121.42.187.97");
-        servSockAddr.sin_port = htons(80);
+        if(get_requests_info(buffer, ip, &port)!=1&&port!=80)
+        {
+            send(clntSock, "https ----- ignore\n", 30 , 0);
+        }
+        else
+        {
+            //服务器发送请求
+            SOCKET sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+            cout << "****************ip:" << ip <<"\tport:" <<port<<endl;
 
-        connect(sock, (SOCKADDR*)&servSockAddr, sizeof(SOCKADDR));
-        send(sock, buffer, strlen(buffer), 0);
+            sockaddr_in servSockAddr;
+            memset(&servSockAddr, 0, sizeof(servSockAddr));
+            servSockAddr.sin_family = PF_INET;
+            servSockAddr.sin_addr.s_addr = inet_addr(ip);
+            servSockAddr.sin_port = htons(80);
 
-        //接收服务器传回的数据
-        int str_len = recv(sock, buf_recv, BUF_SIZE, 0);
-        //输出接收到的数据
+            connect(sock, (SOCKADDR*)&servSockAddr, sizeof(SOCKADDR));
+            send(sock, buffer, strlen(buffer), 0);
+            //接收服务器传回的数据
+            int str_len = recv(sock, buf_recv, BUF_SIZE, 0);
 
-        send(clntSock, buf_recv, str_len, 0);  //将数据返回给本地
+            cout << buf_recv;
+            //将数据返回给本地
+            send(clntSock, buf_recv, str_len, 0);
 
-        closesocket(sock);
-        closesocket(clntSock);  //关闭套接字
-        memset(buffer, 0, BUF_SIZE);  //重置缓冲区
+            //关闭套接字
+            closesocket(sock);
+        }
+        closesocket(clntSock);
+        //重置缓冲区
+        memset(buffer, 0, BUF_SIZE);
         memset(buf_recv, 0, BUF_SIZE);
+        memset(ip, 0, 30);
     }
 
     //关闭套接字
